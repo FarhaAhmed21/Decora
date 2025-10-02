cc24a55ab2b047b82629c0eeae30dc8a41ac7036
FarhaAhmed21 <fzaki6400@gmail.com>
2025-10-03 01:32:52 +0300

    Finish Categories Screen and Add thems files

--- lib/src/features/categories/screens/categories_screen.dart
+++ lib/src/features/categories/screens/categories_screen.dart
@@ -1,93 +1,55 @@
-import 'package:decora/src/features/categories/models/category_model.dart';
-import 'package:decora/src/features/categories/widgets/category_card.dart';
+import 'package:decora/src/shared/components/appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:decora/src/features/categories/models/category_model.dart';
+import 'package:decora/src/features/categories/repository/categoriy_repo.dart';
+import 'package:decora/src/features/categories/widgets/category_card.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  CategoriesScreen({super.key});
+  const CategoriesScreen({super.key});
 
-  final TextEditingController _controller = TextEditingController();
-  final List<CategoryModel> categories = [
-    CategoryModel(
-      id: '1',
-      name: 'Living Room',
-      imageUrl: 'assets/images/living_room.png',
-    ),
-    CategoryModel(
-      id: '2',
-      name: 'Bedroom',
-      imageUrl: 'assets/images/bedroom.png',
-    ),
-    CategoryModel(
-      id: '3',
-      name: 'Dining Room',
-      imageUrl: 'assets/images/kitchen.png',
-    ),
-    CategoryModel(
-      id: '4',
-      name: 'Office',
-      imageUrl: 'assets/images/bathroom.png',
-    ),
-    CategoryModel(
-      id: '5',
-      name: 'Outdoor Room',
-      imageUrl: 'assets/images/dining_room.png',
-    ),
-    CategoryModel(
-      id: '6',
-      name: 'Kids Room',
-      imageUrl: 'assets/images/office.png',
-    ),
-    CategoryModel(
-      id: '6',
-      name: 'Decor & Accessories',
-      imageUrl: 'assets/images/office.png',
-    ),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Our Categories'),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.arrow_back_ios_new),
-        ),
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              TextField(
-                controller: _controller,
-                decoration: InputDecoration(
-                  hintText: 'Search Furniture',
-                  prefixIcon: const Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const CustomAppBar(title: "Our Categories"),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 650,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        return buildGridView(
+                          categories: CategoriyRepo.categories,
+                        );
+                      },
+                    ),
                   ),
-                ),
+                ],
               ),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
-            ],
-          ),
-          const SizedBox(height: 16),
-          SizedBox(
-            height: 300,
-            child: GridView.count(
-              crossAxisCount: 2,
-              mainAxisSpacing: 8,
-              crossAxisSpacing: 8,
-              children: List.generate(6, (index) {
-                return CategoryCard(
-                  title: categories[index].name,
-                  img: categories[index].imageUrl,
-                );
-              }),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget buildGridView({required List<CategoryModel> categories}) {
+  return GridView.count(
+    crossAxisCount: 2,
+    childAspectRatio: 0.97,
+    mainAxisSpacing: 8,
+    crossAxisSpacing: 8,
+    children: List.generate(categories.length, (index) {
+      return CategoryCard(
+        title: categories[index].name,
+        img: categories[index].imageUrl,
+        onTap: () {},
+      );
+    }),
+  );
 }
