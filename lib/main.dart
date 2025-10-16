7fd74150ee806c4aefd01a1ea6eba26a9a03d093
FarhaAhmed21 <fzaki6400@gmail.com>
2025-10-16 14:01:08 +0300

    add blugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:decora/core/l10n/app_localizations.dart';
 import 'package:decora/core/l10n/local_cubit.dart';
+import 'package:decora/firebase_options.dart';
 import 'package:decora/src/features/splash/cubit/splash_cubit.dart';
 import 'package:decora/src/features/splash/screens/splash_screen.dart';
 import 'package:decora/src/shared/theme/app_colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  print('🔥 Firebase initialized successfully');
   runApp(
     MultiBlocProvider(
       providers: [
