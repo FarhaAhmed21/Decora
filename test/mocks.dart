e4c2f407f0b55dcb82563f096a95c6ad2cf613fc
Othman-El-Attar <Othman.nasser274@gmail.com>
2025-11-12 19:36:13 +0200

    Refactor auth integration test: Migrate test from test/testAuth/integration_test to test/integration_test and remove redundant file

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,4 +1,4 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:decora/src/features/Auth/services/firestore_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -7,22 +7,33 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
+/// Combine all mocks here so mockito generates a single `.mocks.dart` file
 @GenerateMocks([
+  // Firebase Auth & related classes
   FirebaseAuth,
   User,
   UserCredential,
+
+  // Google Sign-In
   GoogleSignIn,
   GoogleSignInAccount,
   GoogleSignInAuthentication,
+
+  // Facebook Auth
   FacebookAuth,
   LoginResult,
   AccessToken,
+
+  // Firestore
+  FirebaseFirestore,
+  CollectionReference,
+  DocumentReference,
+  QuerySnapshot,
+  QueryDocumentSnapshot,
+
+  // Custom service
   FirestoreService,
 ])
 class MockBuildContext extends Mock implements BuildContext {}
 
-import 'package:mockito/annotations.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-@GenerateMocks([FirebaseFirestore, CollectionReference, DocumentReference])
 void main() {}
